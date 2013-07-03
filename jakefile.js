40ee048052708fee957f605e333209b247bc6ef4
Juan Gabardini <jgabardini@computer.org>
2013-07-03 08:49:53 -0300

    Jake

---
+++ jakefile.js
@@ -0,0 +1,15 @@
+task("default", function() {
+	console.log("default");
+});
+
+
+desc("Example!");
+task("example", ["dependency"], function() {
+	console.log("example task");
+});
+
+task("dependency", function() {
+	console.log("dependencia task");
+});;
+
+
