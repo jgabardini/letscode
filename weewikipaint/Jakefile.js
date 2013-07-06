f89b7d8da69e324ffc5a76e96962bfbbde8e99b8
Juan Gabardini <jgabardini@kleer.la>
2013-07-06 11:17:03 -0300

    1er test corriendo

--- weewikipaint/Jakefile.js
+++ weewikipaint/Jakefile.js
@@ -21,7 +21,8 @@ task("lint", [], function() {
 
 desc("test everithing");
 task("test", [], function() {
-    console.log("test goes here");
+    var reporter = require("nodeunit").reporters["default"];
+    reporter.run(["src/server/_server_test.js"]);
 });
 
 desc("Integration");
--- weewikipaint/src/server/server.js
+++ weewikipaint/src/server/server.js
@@ -1,1 +1,4 @@
+"use strict";
+
 console.log("Hi, I'm a server. No, really");
+
