a9ba3b91d360374e7c9c7e4cdd915ed6e13d680b
Juan Gabardini <jgabardini@kleer.la>
2013-07-07 23:54:03 -0300

    spike de server node

---
+++ weewikipaint/spikes/node_http/http_server.js
@@ -0,0 +1,15 @@
+
+var http = require("http");
+
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+	console.log("Received request");
+	var body = "<http><head><title>Spike HTTP server</title</head>" +
+		"<body><p><Ejemplo de server HTTP usando node.js/p></body></http>"
+	response.end(body);
+});
+
+server.listen(8080);
+
+console.log("Server started");
