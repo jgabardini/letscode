d84bb51abc16c6f1b9bdbbf228d5cd8c40c6b81c
Juan Gabardini <jgabardini@kleer.la>
2013-07-09 08:22:54 -0300

    pruebas reales del servidor y node 0.10

--- weewikipaint/src/server/_server_test.js
+++ weewikipaint/src/server/_server_test.js
@@ -1,8 +1,20 @@
 "use strict";
 
 var server = require("./server.js");
+var http = require("http");
 
-exports.testNothing = function(test) {
-    test.equals(3, server.number(), "number");
-    test.done();
-}
+exports.tearDown = function (done) {
+	server.stop(function() {
+		console.log("tearDown");
+		done();
+	});
+	//TODO: manejar el caso que llamo a stopt antes que start
+};
+//TODO: test-drven stop callback
+exports.testerverRespondsToGetrequests = function(test) {
+	server.start();
+	http.get("http://localhost:8080/", function(response) {
+		response.on("data", function(){})
+		test.done();
+	});
+};
--- weewikipaint/src/server/server.js
+++ weewikipaint/src/server/server.js
@@ -1,5 +1,19 @@
 "use strict";
 
-exports.number = function() {
-    return 3;
-};
+var http = require("http");
+var server;
+
+exports.start = function () {
+	server = http.createServer();
+
+	server.on("request", function(request, response) {
+		var body = "<http><head><title>Spike HTTP server</title</head>" +
+			"<body><p><Ejemplo de server HTTP usando node.js/p></body></http>"
+		response.end(body);
+	});
+	server.listen(8080);	//TODO: remove duplication 
+}
+
+exports.stop = function (callback) {
+	server.close(callback);
+}
